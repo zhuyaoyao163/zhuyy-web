6b66c3a1a95799f983d652013903c1b56aaa8134
zhuyy <zhuyy@bbtree.com>
2016-06-14 15:09:29 +0800

    Revert: 。。。。。。

--- src/main/java/com/zhuyy/demo/mapper/UserMapper.java
+++ src/main/java/com/zhuyy/demo/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.zhuyy.demo.mapper;
 
 import com.zhuyy.demo.model.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +15,6 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    User selectUserForLogin(@Param("loginName") String loginName,@Param("passWord") String passWord);
 }
